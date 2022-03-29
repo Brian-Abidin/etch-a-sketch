50a44e40eea48855fd6f753740cc4006014ae3e9
Brian Abidin <Brian.abidin1@gmail.com>
2022-03-29 14:55:54 -0400

    add text to README.Add eventlisteners for mouseover

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+The Odin Project - Etch-A-Sketch 
+
+Create webpage displaying an interactive etch-a-sketch using mainly DOM methods in JavaScript.
--- etch.js
+++ etch.js
@@ -8,6 +8,25 @@ function makeGrid(){
             row.appendChild(box); //adds j boxes to each row 
         }
         document.getElementById('boxParent').appendChild(row); //puts all divs inside boxParent div
-    }    
+    } 
 }
-window.onload = makeGrid; //when page load run makeGrid
+// window.onload = makeGrid; when page load run makeGrid
+
+window.onload = function(){
+    makeGrid();
+    let box = document.getElementsByClassName("box");
+    box[0].addEventListener("mouseover", func, false);
+    box[0].addEventListener("mouseout", func1, false);
+    function func(){
+        let box = document.getElementsByClassName("box");
+        for(let i = 0; i < box.length; i++){
+            box[i].setAttribute("style", "background-color:blue;");
+        }
+    }
+    function func1(){
+        let box = document.getElementsByClassName("box");
+        for(let i = 0; i < box.length; i++){
+            box[i].setAttribute("style", "background-color:red;");
+        }
+    }
+}
