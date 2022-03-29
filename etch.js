cc887079273396b86bae42185d6d9e6eb875d767
Brian Abidin <Brian.abidin1@gmail.com>
2022-03-29 12:18:47 -0400

    add comments to etch

--- etch.js
+++ etch.js
@@ -1,13 +1,13 @@
 function makeGrid(){
     for(let i=0; i<16; i++){
-        let row = document.createElement('div');
-        row.className = "row";
+        let row = document.createElement('div'); // creates i amount of row
+        row.className = "row"; // div given class name row
         for(let j=0; j<16; j++){
-            let box = document.createElement('div');
-            box.className = "box";
-            row.appendChild(box);
+            let box = document.createElement('div'); //creates j amount of boxes
+            box.className = "box"; // div given class name box
+            row.appendChild(box); //adds j boxes to each row 
         }
-        document.getElementById('boxParent').appendChild(row);
+        document.getElementById('boxParent').appendChild(row); //puts all divs inside boxParent div
     }    
 }
-window.onload = makeGrid;
+window.onload = makeGrid; //when page load run makeGrid
