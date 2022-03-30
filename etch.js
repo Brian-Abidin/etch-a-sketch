867968297623a6ada5fe192843603831d26e61e2
Brian Abidin <Brian.abidin1@gmail.com>
2022-03-30 15:05:57 -0400

    update grid function code and logic

--- etch.js
+++ etch.js
@@ -1,16 +1,44 @@
-function makeGrid(){
-    for(let i=0; i<20; i++){
+const boxParent = document.querySelector('#boxParent')
+
+/* function makeGrid(col, row){
+    for(let i=0; i<36; i++){
         let row = document.createElement('div'); // creates i amount of row
         row.className = "row"; // div given class name row
-        for(let j=0; j<20; j++){
+        for(let j=0; j<36; j++){
             let box = document.createElement('div'); //creates j amount of boxes
             box.className = "box"; // div given class name box
             row.appendChild(box); //adds j boxes to each row 
         }
         document.getElementById('boxParent').appendChild(row); //puts all divs inside boxParent div
     } 
+} */
+
+function nameTitle(){
+    const title = document.querySelector('h1');
+    title.textContent = "Etch-A-Sketch";
 }
-window.onload = function(){ 
+
+function makeGrid(col, row){
+    for(let i = 0; i<(col*row); i++){
+        const row = document.createElement('div');
+        row.style.border = '1px solid black';
+        boxParent.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        boxParent.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+        boxParent.appendChild(row).classList.add('box');
+    }
+}
+
+window.onload = function(){
+    nameTitle();
+    makeGrid(16,16);
+    let box = document.getElementsByClassName("box"); 
+    Array.from(box).forEach(etch => etch.addEventListener('mouseover', function(){ //changes variable into array and turns mouseover boxes background color black
+        etch.style.background = 'black';
+    }));
+}
+
+
+/* window.onload = function(){ 
     makeGrid();// when page load run makeGrid
     let box = document.getElementsByClassName("box"); 
     Array.from(box).forEach(etch => etch.addEventListener('mouseover', function(){ //changes variable into array and turns mouseover boxes background color black
@@ -20,4 +48,5 @@ window.onload = function(){
 const resetButton = document.createElement('button');
 resetButton.textContent = "Reset Grid";
 resetButton.setAttribute('onclick', "alert('hello!');");
-container.appendChild(resetButton);
+container.appendChild(resetButton); */
+
